db3cf38947f91eb660294416d0e46818501b5248
NikaKiknadze <kiknadzenika88@gmail.com>
2023-11-19 00:33:08 +0400

    Added Repositories.

---
+++ UniversityApi/Repositories/CourseRepository.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApi.Data;
+using UniversityApi.Entities;
+
+namespace UniversityApi.Repositories
+{
+    public class CourseRepository
+    {
+        private readonly UniversistyContext _context;
+        public CourseRepository(UniversistyContext context)
+        {
+            _context = context;
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public Course GetCourseById(int courseId)
+        {
+            return _context.Courses
+                .Include(c => c.UsersCourses)
+                .Include(c => c.CoursesLecturers)
+                .Include(c => c.Faculty)
+                .FirstOrDefault(c => c.Id == courseId);
+        }
+
+        public IQueryable<Course> GetCourses()
+        {
+            return _context.Courses.AsQueryable();
+        }
+
+        public User CreateCourse(Course course)
+        {
+            _context.Courses.Add(course);
+            return course;
+        }
+
+        public bool UpdateCourse(Course updatedCourse)
+        {
+            var existingCourse = _context.Courses.FirstOrDefault(c => c.Id == updatedCourse.Id);
+
+            if (existingCourse == null)
+            {
+                return false;
+            }
+
+            existingCourse.CourseName = updatedCourse.CourseName;
+            existingCourse.FacultyId = updatedCourse.FacultyId;
+            return true;
+        }
+
+        public bool DeleteCourse(int courseId)
+        {
+            var course = _context.Courses.FirstOrDefault(c => c.Id == courseId);
+
+            if (course == null)
+            {
+                return false;
+            }
+
+            _context.Courses.Remove(course);
+            return true;
+        }
+
+        public bool DeleteUsersCourses(int courseId)
+        {
+            var usersCourses = _context.UsersCoursesJoin
+                                       .Where(c => c.CourseId == courseId);
+            if (usersCourses == null)
+            {
+                return false;
+            }
+            _context.UsersCoursesJoin.RemoveRange(usersCourses);
+            return true;
+        }
+
+        public bool DeleteCourseLecturers(int courseId)
+        {
+            var courseLecturers = _context.CoursesLecturersJoin
+                                         .Where(c => c.CourseId == courseId);
+            if (courseLecturers == null)
+            {
+                return false;
+            }
+            _context.CoursesLecturersJoin.RemoveRange(courseLecturers);
+            return true;
+        }
+    }
+}
